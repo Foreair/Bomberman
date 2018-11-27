ade37f0b01a8c9bce1dc2bafdb9c22615e401b78
lord semi <josemi.masip@gmail.com>
2018-11-27 03:39:04 +0100

    FEATURE: Spawning bombs as childs of the player and correctly updating the bomb counter. Updated tileset for the map and deleted unnecessary tiles.

--- Bomberman/Assets/Scripts/PlayerController.cs
+++ Bomberman/Assets/Scripts/PlayerController.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
-    public float speed;
+    [Header("Player Variables")]
+    [Tooltip("Player's movement speed")]
+    public float speed = 5;
+    [Tooltip("Maximum number of bombs the player can deploy")]
+    public int maxBombs = 1;
+    //[HideInInspector]
+    public int currentBombs = 0;
     private Vector2 endPosition2d;
     private bool moving;
     private Rigidbody2D rb2d;
     private Vector2 movement = Vector2.zero;
+    [Space]
     public GameObject Bomb;
+    public Grid grid;
 
+    
     private void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
@@ -23,7 +31,7 @@ public class PlayerController : MonoBehaviour
     {
         UpdateMovement();
         UpdateMechanics();
-        
+
     }
 
 
@@ -83,14 +91,16 @@ public class PlayerController : MonoBehaviour
 
     private void UpdateMechanics()
     {
-        if (Input.GetButton("Jump"))
+        if (Input.GetButtonDown("Jump") && currentBombs < maxBombs)
         {
-            Instantiate(Bomb, transform);
+
+            GameObject instance = Instantiate(Bomb, SnapBomb(transform.position), Bomb.transform.rotation);
+            instance.transform.parent = transform;
+            currentBombs++;
         }
     }
 
-
-    private bool IsMoving(Vector2 movement)
+    public bool IsMoving(Vector2 movement)
     {
         if (movement.x == 0 && movement.y == 0)
         {
@@ -102,4 +112,33 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    private Vector3 SnapBomb(Vector3 pos)
+    {
+        Vector3 snappedPos = Vector3.zero;
+        if(pos.x > 0)
+        {
+            snappedPos.x = (int) pos.x + (grid.cellSize.x/2);
+        }
+        else
+        {
+            snappedPos.x = (int)pos.x - (grid.cellSize.x / 2);
+        }
+
+        if (pos.y > 0)
+        {
+            snappedPos.y = (int)pos.y + (grid.cellSize.y / 2);
+        }
+        else
+        {
+            snappedPos.y = (int)pos.y - (grid.cellSize.y / 2);
+        }
+
+        return snappedPos;
+    }
+
+    //[UnityEditor.MenuItem("Tools/Increase Speed")]
+    //public static void IncreaseSpeed()
+    //{
+    //    speed++;
+    //}
 }
